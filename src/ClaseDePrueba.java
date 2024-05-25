273a823473a17c8f15e55cc4646e88bba5b1addf
yeiyei09 <yeffergomez02@gmail.com>
2024-05-25 00:08:09 -0500

    Yefferson puse print main

--- src/ClaseDePrueba.java
+++ src/ClaseDePrueba.java
@@ -1,5 +1,6 @@
 public class ClaseDePrueba {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Este es el repositorio");
     }
 }
